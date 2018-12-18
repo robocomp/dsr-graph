fc41d8f8669a531b5c09b8777d9a5cedf6452647
Pablo Bustos <pbustos@unex.es>
2018-12-18 18:38:18 +0100

    now saved positions are recuperadas

--- components/newagent/src/specificworker.cpp
+++ components/newagent/src/specificworker.cpp
@@ -59,187 +59,11 @@ bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
 	return true;
 }
 
-
-
-// void SpecificWorker::initializeXML(std::string file_name)
-// {
-// 	//version = 0;
-// 	std::cout << __FUNCTION__ << "Reading xml file: " << file_name << std::endl;
-
-// 	// Open file and make initial checks
-// 	xmlDocPtr doc;
-// 	if ((doc = xmlParseFile(file_name.c_str())) == NULL)
-// 	{
-// 		fprintf(stderr,"Can't read XML file - probably a syntax error. \n");
-// 		exit(1);
-// 	}
-// 	xmlNodePtr root;
-// 	if ((root = xmlDocGetRootElement(doc)) == NULL)
-// 	{
-// 		fprintf(stderr,"Can't read XML file - empty document\n");
-// 		xmlFreeDoc(doc);
-// 		exit(1);
-// 	}
-// 	if (xmlStrcmp(root->name, (const xmlChar *) "AGMModel"))
-// 	{
-// 		fprintf(stderr,"Can't read XML file - root node != AGMModel");
-// 		xmlFreeDoc(doc);
-// 		exit(1);
-// 	}
-
-// 	// Read symbols (just symbols, then links in other loop)
-// 	for (xmlNodePtr cur=root->xmlChildrenNode; cur!=NULL; cur=cur->next)
-// 	{
-// 		if (xmlStrcmp(cur->name, (const xmlChar *)"symbol") == 0)
-// 		{
-// 			xmlChar *stype = xmlGetProp(cur, (const xmlChar *)"type");
-// 			xmlChar *sid = xmlGetProp(cur, (const xmlChar *)"id");
-			
-// 			//AGMModelSymbol::SPtr s = newSymbol(atoi((char *)sid), (char *)stype);
-// 			IDType node_id = std::atoi((char *)sid);
-// 			std::string node_type((char *)stype);
-// 			graph->addNode(node_id, node_type);
-// 			graph->addNodeAttribs(node_id, DSR::Attribs{ 
-// 								//std::pair("name", node_type), 
-// 								//std::pair("type", node_type),
-// 								std::pair("level", std::int32_t(0)),
-// 								std::pair("parent", IDType(0))
-// 				});
-	
-// 			// Draw attributes come now
-// 			DSR::DrawAttribs atts;
-// 			std::string qname = (char *)stype;
-// 			std::string full_name = std::string((char *)stype) + " [" + std::string((char *)sid) + "]";
-// 			auto rd = QVec::uniformVector(2,-200,200);
-// 			atts.insert(std::pair("name", full_name));
-// 			atts.insert(std::pair("posx", rd[0]));
-// 			atts.insert(std::pair("posy", rd[1]));
-			
-// 			// color selection
-// 			std::string color = "coral";
-// 			if(qname == "world") color = "SeaGreen";
-// 			else if(qname == "transform") color = "SteelBlue";
-// 			else if(qname == "plane") color = "Khaki";
-// 			else if(qname == "differentialrobot") color = "GoldenRod";
-// 			else if(qname == "laser") color = "GreenYellow";
-// 			else if(qname == "mesh") color = "LightBlue";
-// 			else if(qname == "imu") color = "LightSalmon";
-			
-			
-			
-			
-// 			atts.insert(std::pair("color", color));
-// 			graph->addNodeDrawAttribs(node_id, atts);
-// 			std::cout << node_id << " " <<  std::string((char *)stype) << std::endl;
-			
-// 			xmlFree(sid);
-// 			xmlFree(stype);
-
-// 			for (xmlNodePtr cur2=cur->xmlChildrenNode; cur2!=NULL; cur2=cur2->next)
-// 			{
-// 				if (xmlStrcmp(cur2->name, (const xmlChar *)"attribute") == 0)
-// 				{
-// 					xmlChar *attr_key   = xmlGetProp(cur2, (const xmlChar *)"key");
-// 					xmlChar *attr_value = xmlGetProp(cur2, (const xmlChar *)"value");
-					
-// 					//s->setAttribute(std::string((char *)attr_key), std::string((char *)attr_value));
-// 					graph->addNodeAttribs(node_id, DSR::Attribs{ std::pair(std::string((char *)attr_key), std::string((char *)attr_value))});
-					
-// 					xmlFree(attr_key);
-// 					xmlFree(attr_value);
-// 				}
-// 				else if (xmlStrcmp(cur2->name, (const xmlChar *)"comment") == 0) { }           // coments are always ignored
-// 				else if (xmlStrcmp(cur2->name, (const xmlChar *)"text") == 0) { }     // we'll ignore 'text'
-// 				else { printf("unexpected tag inside symbol: %s\n", cur2->name); exit(-1); } // unexpected tags make the program exit
-// 			}
-// 		}
-// 		else if (xmlStrcmp(cur->name, (const xmlChar *)"link") == 0) { }     // we'll ignore links in this first loop
-// 		else if (xmlStrcmp(cur->name, (const xmlChar *)"text") == 0) { }     // we'll ignore 'text'
-// 		else if (xmlStrcmp(cur->name, (const xmlChar *)"comment") == 0) { }  // coments are always ignored
-// 		else { printf("unexpected tag #1: %s\n", cur->name); exit(-1); }      // unexpected tags make the program exit
-// 	}
-
-// 	// Read links
-// 	for (xmlNodePtr cur=root->xmlChildrenNode; cur!=NULL; cur=cur->next)
-// 	{
-// 		if (xmlStrcmp(cur->name, (const xmlChar *)"link") == 0)
-// 		{
-// 			xmlChar *srcn = xmlGetProp(cur, (const xmlChar *)"src");
-// 			if (srcn == NULL) { printf("Link %s lacks of attribute 'src'.\n", (char *)cur->name); exit(-1); }
-// 			IDType a = atoi((char *)srcn);
-// 			xmlFree(srcn);
-
-// 			xmlChar *dstn = xmlGetProp(cur, (const xmlChar *)"dst");
-// 			if (dstn == NULL) { printf("Link %s lacks of attribute 'dst'.\n", (char *)cur->name); exit(-1); }
-// 			IDType b = atoi((char *)dstn);
-// 			xmlFree(dstn);
-
-// 			xmlChar *label = xmlGetProp(cur, (const xmlChar *)"label");
-// 			if (label == NULL) { printf("Link %s lacks of attribute 'label'.\n", (char *)cur->name); exit(-1); }
-// 			std::string edgeName((char *)label);
-// 			xmlFree(label);
-
-// 			std::map<std::string, std::string> attrs;
-// 			for (xmlNodePtr cur2=cur->xmlChildrenNode; cur2!=NULL; cur2=cur2->next)
-// 			{
-// 				if (xmlStrcmp(cur2->name, (const xmlChar *)"linkAttribute") == 0)
-// 				{
-// 					xmlChar *attr_key   = xmlGetProp(cur2, (const xmlChar *)"key");
-// 					xmlChar *attr_value = xmlGetProp(cur2, (const xmlChar *)"value");
-// 					attrs[std::string((char *)attr_key)] = std::string((char *)attr_value);
-// 					xmlFree(attr_key);
-// 					xmlFree(attr_value);
-// 				}
-// 				else if (xmlStrcmp(cur2->name, (const xmlChar *)"comment") == 0) { }           // coments are always ignored
-// 				else if (xmlStrcmp(cur2->name, (const xmlChar *)"text") == 0) { }     // we'll ignore 'text'
-// 				else { printf("unexpected tag inside symbol: %s ==> %s\n", cur2->name,xmlGetProp(cur2, (const xmlChar *)"id") ); exit(-1); } // unexpected tags make the program exit
-// 			}
-			
-// 			graph->addEdge(a, b, edgeName);
-//  			//graph->addEdgeAttribs(a, b, DSR::Attribs{std::pair("name", edgeName)});
-// 			graph->addEdgeDrawAttribs(a, b, DSR::DrawAttribs{std::pair("name", edgeName)});
-//  			DSR::Attribs edge_attribs;
-// 			if( edgeName == "RT")   //add level to node b as a.level +1, and add parent to node b as a
-// 			{ 	
-// 				graph->addNodeAttribs(b, DSR::Attribs{ std::pair("level", graph->getNodeLevel(a)+1), std::pair("parent", a)});
-// 				RMat::RTMat rt;
-// 				float x,y,z;
-// 				for(auto &[k,v] : attrs)
-// 				{
-// 					if(k=="tx")	( x = std::stof(v) );
-// 					if(k=="ty")	( y = std::stof(v) );
-// 					if(k=="tz")	( z = std::stof(v) );
-// 					rt.setTr( x, y, z);
-// 					if(k=="rx")	rt.setRX(std::stof(v));
-// 					if(k=="ry")	rt.setRY(std::stof(v));
-// 					if(k=="rz")	rt.setRZ(std::stof(v));
-//  					graph->addEdgeAttribs(a, b, DSR::Attribs{std::pair("RT", rt)});
-// 				}
-// 			}
-// 			else
-// 			{
-// 				graph->addNodeAttribs(b, DSR::Attribs{ std::pair("parent", 0)});
-// 				for(auto &r : attrs)
-//  					edge_attribs.insert(r);
-// 			}
-
-// 			std::cout << __FUNCTION__ << "edge  from " << a << " " <<  edgeName  << std::endl;		
-//  			graph->addEdgeAttribs(a, b, edge_attribs);
-			
-// 	//	  graph->addEdgeByIdentifiers(a, b, edgeName, attrs);
-// 		}
-// 		else if (xmlStrcmp(cur->name, (const xmlChar *)"symbol") == 0) { }   // symbols are now ignored
-// 		else if (xmlStrcmp(cur->name, (const xmlChar *)"text") == 0) { }     // we'll ignore 'text'
-// 		else if (xmlStrcmp(cur->name, (const xmlChar *)"comment") == 0) { }  // comments are always ignored
-// 		else { printf("unexpected tag #2: %s\n", cur->name); exit(-1); }      // unexpected tags make the program exit
-// 	}
-	
-// }
-
 void SpecificWorker::initialize(int period)
 {
 	graph = std::make_shared<DSR::Graph>();
-	graph->readFromFile("/home/robocomp/robocomp/files/innermodel/simpleworld-hybrid.xml");
+	//graph->readFromFile("/home/robocomp/robocomp/files/innermodel/simpleworld-hybrid.xml");
+	graph->readFromFile("caca.xml");
 	graph->print();
 	
 	std::cout << __FILE__ << __FUNCTION__ << " -- Initializing graphic graph" << std::endl;
@@ -251,8 +75,6 @@ void SpecificWorker::initialize(int period)
 	connect(&graph_viewer, SIGNAL(saveGraphSIGNAL()), this, SLOT(saveGraphSLOT()));
 					
 	drawGraph();
-	
-
 	std::cout << __FILE__ << __FUNCTION__ << " -- Graph set OK" << std::endl;
 	//graph_viewer.show();	
 	std::cout << __FILE__ << __FUNCTION__ << " -- Graph shown OK" << std::endl;
@@ -301,8 +123,8 @@ void SpecificWorker::drawGraph()
 	for(const auto &par : *graph)
 	{
 		const auto &node_id = par.first;
-		float node_posx = graph->getNodeDrawAttribByName<float>(node_id, "posx");
-		float node_posy = graph->getNodeDrawAttribByName<float>(node_id, "posy");
+		float node_posx = graph->getNodeAttribByName<float>(node_id, "pos_x");
+		float node_posy = graph->getNodeAttribByName<float>(node_id, "pos_y");
 		std::string color_name = graph->getNodeDrawAttribByName<std::string>(node_id, "color");
 		std::string qname = graph->getNodeDrawAttribByName<std::string>(node_id, "name");
 		std::string type = graph->getNodeType(node_id);
