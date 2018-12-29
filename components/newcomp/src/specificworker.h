376b0957d386cbb42829c648e413d73d556f9263
pbustos <pbustos@unex.es>
2018-12-29 17:29:23 +0100

    modificaciones para tener un subscriptor en un hilo y publicar desde el compute una estructura compleja definida en DSRGraph.ice

--- components/newcomp/src/specificworker.h
+++ components/newcomp/src/specificworker.h
@@ -1,35 +1,14 @@
-/*
- *    Copyright (C)2018 by YOUR NAME HERE
- *
- *    This file is part of RoboComp
- *
- *    RoboComp is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    RoboComp is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
-       \brief
-       @author authorname
-*/
-
-
-
 #ifndef SPECIFICWORKER_H
 #define SPECIFICWORKER_H
 
 #include <genericworker.h>
 #include <innermodel/innermodel.h>
 #include <DataStorm/DataStorm.h>
+#include "src/DSRGraph.h"
+#include <chrono>
+#include <thread>
+
+using G = RoboCompDSR::DSRGraph;
 
 class SpecificWorker : public GenericWorker
 {
@@ -38,8 +17,7 @@ public:
 	SpecificWorker(TuplePrx tprx);
 	~SpecificWorker();
 	bool setParams(RoboCompCommonBehavior::ParameterList params);
-
-
+	
 public slots:
 	void compute();
 	void initialize(int period);
@@ -47,7 +25,19 @@ public slots:
 private:
 	InnerModel *innerModel;
 	string role;
+	DataStorm::Node node;
+	std::shared_ptr<DataStorm::SingleKeyWriter<std::string, G>> writer;
+	std::shared_ptr<DataStorm::Topic<std::string, G>> topic;
+	void subscribeThread();
+	std::thread read_thread;
 
+	DataStorm::SingleKeyWriter<std::string, G>
+	makeGraphWriter(DataStorm::Topic<std::string, G>& topic, std::string sender, G graph)
+	{
+		auto writer = DataStorm::makeSingleKeyWriter(topic, move(sender));
+    	writer.add(move(graph));
+		return writer;
+	}
 };
 
 #endif
