b9c643088db7249e68f5199f70aff6d164e5e561
pbustos <pbustos@unex.es>
2019-01-09 22:17:43 +0100

    working on concurrent api

---
+++ components/concurrent/src/specificworker.cpp
@@ -0,0 +1,64 @@
+
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(TuplePrx tprx) : GenericWorker(tprx)
+{
+
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+//       THE FOLLOWING IS JUST AN EXAMPLE
+//	To use innerModelPath parameter you should uncomment specificmonitor.cpp readConfig method content
+//	try
+//	{
+//		RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+//		std::string innermodel_path = par.value;
+//		innerModel = new InnerModel(innermodel_path);
+//	}
+//	catch(std::exception e) { qFatal("Error reading config params"); }
+	return true;
+}
+
+void SpecificWorker::initialize(int period)
+{
+	std::cout << "Initialize worker" << std::endl;
+
+	// DSR Graph creation
+	graph = std::make_shared<DSR::Graph>();
+	//graph->readFromFile("/home/robocomp/robocomp/files/innermodel/simpleworld-hybrid.xml");
+	graph->readFromFile("caca.xml");
+	graph->print();
+
+	this->Period = period;
+	timer.setSingleShot(true);
+	timer.start(Period);
+}
+
+void SpecificWorker::compute()
+{
+	//Let's start the party
+
+	{
+		auto node = graph->getNodePtr(100);
+		std::cout << node->id << /*node->attrs.at("imType" << " " <<*/ node->type  << std::endl;
+	}
+	
+	auto nodeT = graph->nodes[100];
+	std::cout << "Truth " << nodeT.id << " " << nodeT.type  << std::endl;
+
+}
+
+
+
