cc3f28e5aad4bb617a9042b9c353fe0a776451ab
pbustos <pbustos@unex.es>
2019-01-10 22:13:45 +0100

    debuggin

--- components/concurrent/src/specificworker.cpp
+++ components/concurrent/src/specificworker.cpp
@@ -39,7 +39,7 @@ void SpecificWorker::initialize(int period)
 	graph = std::make_shared<DSR::Graph>();
 	//graph->readFromFile("/home/robocomp/robocomp/files/innermodel/simpleworld-hybrid.xml");
 	graph->readFromFile("caca.xml");
-	graph->print();
+	//graph->print();
 
 	this->Period = period;
 	timer.setSingleShot(true);
@@ -52,9 +52,12 @@ void SpecificWorker::compute()
 
 	{
 		auto node = graph->getNodePtr(100);
-		std::cout << node->id << /*node->attrs.at("imType" << " " <<*/ node->type  << std::endl;
+		std::cout << "line" << std::endl;	
+		std::cout << "id " << node->id /*<< node->attrs.at("imType")*/ << " type " << node->type  << std::endl;
 	}
-	
+
+
+	std::cout << "line" << std::endl;	
 	auto nodeT = graph->nodes[100];
 	std::cout << "Truth " << nodeT.id << " " << nodeT.type  << std::endl;
 
