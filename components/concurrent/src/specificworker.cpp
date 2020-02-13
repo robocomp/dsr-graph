6af69ff7cc744caf85f60ff43a22fe911ff18427
pbustos <pbustos@unex.es>
2020-02-13 23:01:31 +0100

    remove Makefiles

--- components/concurrent/src/specificworker.cpp
+++ components/concurrent/src/specificworker.cpp
@@ -34,6 +34,8 @@ void SpecificWorker::initialize(int period)
 	//graph->readFromFile("/home/robocomp/robocomp/files/innermodel/simpleworld-hybrid.xml");
 	graph->readFromFile("caca.xml");
 	//graph->print();
+	// for(auto [k,v]: *graph)
+	// 	std::cout << k << std::endl;
 
 	//this->Period = period;
 	//timer.setSingleShot(true);
@@ -44,79 +46,153 @@ void SpecificWorker::initialize(int period)
 void SpecificWorker::compute()
 {
 	//Let's start the party
-	// TEST 1
-	{
-		std::cout << "---------------- TEST 1 ----------------" << std::endl;
-		{
-			auto n = graph->getNodePtr(100);
-			auto &node = *(n.get());
-		}  
-		//if scope is removed the second new blocks here
+	// // TEST 1
+	// {
+	// 	std::cout << std::endl;
+	// 	std::cout << "---------------- TEST 1 ----------------" << std::endl;
+	// 	{
+	// 		auto n = graph->getNodePtr(100);
+	// 		auto &node = *(n.get());
+	// 	}  
+	// 	//if scope is removed the second new blocks here
 		
-		std::cout << "waiting" << std::endl;
-		auto n2 = graph->getNodePtr(100);
-		auto &node2 = *(n2.get());
+	// 	std::cout << "waiting" << std::endl;
+	// 	auto n2 = graph->getNodePtr(100);
+	// 	auto &node2 = *(n2.get());
 		
 		
-		std::cout << "id " << node2->id << " type " << node2->type  << std::endl;
-		//std::cout << "did " << node->id << " type " << node->type  << std::endl;
+	// 	std::cout << "id " << node2->id << " type " << node2->type  << std::endl;
+	// 	//std::cout << "did " << node->id << " type " << node->type  << std::endl;
 	
-		std::cout << "line" << std::endl;	
-		auto nodeT = graph->nodes[100];
-		std::cout << "Truth, should be: " << nodeT.id << " " << nodeT.type  << std::endl;
-	}
+	// 	std::cout << "line" << std::endl;	
+	// 	auto nodeT = graph->nodes[100];
+	// 	std::cout << "Truth, should be: " << nodeT.id << " " << nodeT.type  << std::endl;
+	// }
 
-	// TEST 2
-	{
-		std::cout << "---------------- TEST 2 ----------------" << std::endl;
-		auto n = graph->getNode(100);
-		std::cout << "before id " << n.id << " type " << n.type  << std::endl;
-		n.type = "morningsinger";
-		graph->replaceNode(100, n);
-		auto n2 = graph->getNode(100);
-		std::cout << "after change, id " << n2.id << " type " << n2.type  << std::endl;
-	}
+	// // TEST 2
+	// {
+	// 	std::cout << std::endl;
+	// 	std::cout << "---------------- TEST 2 ----------------" << std::endl;
+	// 	auto n = graph->getNode(100);
+	// 	std::cout << "before id " << n.id << " type " << n.type  << std::endl;
+	// 	n.type = "morningsinger";
+	// 	graph->replaceNode(100, n);
+	// 	auto n2 = graph->getNode(100);
+	// 	std::cout << "after change, id " << n2.id << " type " << n2.type  << std::endl;
+	// }
 
-	// TEST 3
+	// // TEST 3
+	// {
+	// 	std::cout << std::endl;
+	// 	std::cout << "---------------- TEST 3 ----------------" << std::endl;
+	// 	std::random_device random;  //Will be used to obtain a seed for the random number engine
+    // 	std::mt19937 gen(random()); //Standard mersenne_twister_engine seeded with rd()
+    // 	std::uniform_int_distribution<> dist(100, 139);
+	// 	auto start = std::chrono::high_resolution_clock::now();
+	// 	for(auto i: iter::range(100000))
+	// 	{
+	// 		auto ind = dist(gen);
+	// 		auto n = graph->getNode(ind);
+	// 		//std::cout << "before id " << n.id << " type " << n.type  << std::endl;
+	// 		n.type = "morningsinger";
+	// 		graph->replaceNode(ind, n);
+	// 		//auto n2 = graph->getNode(ind);
+	// 		//std::cout << "after change, id " << n2.id << " type " << n2.type  << std::endl;
+	// 	}
+	// 	auto finish = std::chrono::high_resolution_clock::now();
+	// 	std::chrono::duration<double> elapsed = finish - start;
+	// 	std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+	// }
+
+	// // TEST 4
+	// {
+	// 	std::cout << std::endl;
+	// 	std::cout << "---------------- TEST 4 ----------------" << std::endl;
+	// 	std::random_device random;  //Will be used to obtain a seed for the random number engine
+    // 	std::mt19937 gen(random()); //Standard mersenne_twister_engine seeded with rd()
+    // 	std::uniform_int_distribution<> dist(100, 139);
+	// 	auto start = std::chrono::high_resolution_clock::now();
+	// 	for(auto i: iter::range(100000))
+	// 	{
+	// 		auto ind = dist(gen);
+	// 		//auto n2 = graph->getNodePtr(ind);
+	// 		auto &node2 = *(graph->getNodePtr(ind).get());
+	// 		//auto &node2 = *(n2.get());
+	// 		node2->type = "morningsinger";
+	// 	}
+	// 	auto finish = std::chrono::high_resolution_clock::now();
+	// 	std::chrono::duration<double> elapsed = finish - start;
+	// 	std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+	// }
+
+	// start with threads
+
+	// TEST 5  N threads editing random nodes in the graph M times each using prx
 	{
-		std::cout << "---------------- TEST 3 ----------------" << std::endl;
-		std::random_device random;  //Will be used to obtain a seed for the random number engine
-    	std::mt19937 gen(random()); //Standard mersenne_twister_engine seeded with rd()
-    	std::uniform_int_distribution<> dist(100, 139);
-		auto start = std::chrono::high_resolution_clock::now();
-		for(auto i: iter::range(100000))
+		std::cout << std::endl;
+		std::cout << "---------------- TEST 5 ----------------" << std::endl;
+		std::vector<std::thread> threads;
+
+		auto lambda = [this]
 		{
-			auto ind = dist(gen);
-			auto n = graph->getNode(ind);
-			//std::cout << "before id " << n.id << " type " << n.type  << std::endl;
-			n.type = "morningsinger";
-			graph->replaceNode(ind, n);
-			//auto n2 = graph->getNode(ind);
-			//std::cout << "after change, id " << n2.id << " type " << n2.type  << std::endl;
-		}
-		auto finish = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> elapsed = finish - start;
-		std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+			std::random_device random;  //Will be used to obtain a seed for the random number engine
+			std::mt19937 gen(random()); //Standard mersenne_twister_engine seeded with rd()
+			std::uniform_int_distribution<> dist(100, 139);
+			std::uniform_int_distribution<> delay(1, 50);
+			auto start = std::chrono::high_resolution_clock::now();
+			for(auto i: iter::range(10000))
+			{
+				auto ind = dist(gen);
+				auto &n = *(graph->getNodePtr(ind).get());
+				//auto n = graph->getNode(ind);
+				n->type = "morningsinger";
+				//std::this_thread::sleep_for(std::chrono::milliseconds(delay(gen)));
+				//graph->replaceNode(ind, n);
+			}
+			auto finish = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> elapsed = finish - start;
+			std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+		};
+
+		for(auto i : iter::range(10))
+			threads.push_back(std::thread(lambda));
+		std::cout << "threads initiated" << std::endl;
+		for(auto &t : threads)
+			t.join();
+		std::cout << "threads finished" << std::endl;
 	}
 
-	// TEST 4
+	// TEST 6  N threads editing random nodes in the graph M times each using cpy
 	{
-		std::cout << "---------------- TEST 4 ----------------" << std::endl;
-		std::random_device random;  //Will be used to obtain a seed for the random number engine
-    	std::mt19937 gen(random()); //Standard mersenne_twister_engine seeded with rd()
-    	std::uniform_int_distribution<> dist(100, 139);
-		auto start = std::chrono::high_resolution_clock::now();
-		for(auto i: iter::range(100000))
+		std::cout << std::endl;
+		std::cout << "---------------- TEST 6 ----------------" << std::endl;
+		std::vector<std::thread> threads;
+
+		auto lambda = [this]
 		{
-			auto ind = dist(gen);
-			//auto n2 = graph->getNodePtr(ind);
-			auto &node2 = *(graph->getNodePtr(ind).get());
-			//auto &node2 = *(n2.get());
-			node2->type = "morningsinger";
-		}
-		auto finish = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> elapsed = finish - start;
-		std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+			std::random_device random;  //Will be used to obtain a seed for the random number engine
+			std::mt19937 gen(random()); //Standard mersenne_twister_engine seeded with rd()
+			std::uniform_int_distribution<> dist(100, 139);
+			std::uniform_int_distribution<> delay(1, 50);
+			auto start = std::chrono::high_resolution_clock::now();
+			for(auto i: iter::range(10000))
+			{
+				auto ind = dist(gen);
+				auto n = graph->getNode(ind);
+				n.type = "morningsinger";
+				graph->replaceNode(ind, n);
+			}
+			auto finish = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> elapsed = finish - start;
+			std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+		};
+
+		for(auto i : iter::range(10))
+			threads.push_back(std::thread(lambda));
+		std::cout << "threads initiated" << std::endl;
+		for(auto &t : threads)
+			t.join();
+		std::cout << "threads finished" << std::endl;
 	}
 }
 
