29ce1179bcbb81ad7b408da8c82bb2792a599868
pbustos <pbustos@unex.es>
2020-03-21 18:16:07 +0100

    Fast RTPS integrado

---
+++ components/crdt_rtps_dsr/src/fast_rtps/dsrparticipant.h
@@ -0,0 +1,46 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*!
+ * @file particpant.h
+ * This header file contains the declaration of the participant functions.
+ */
+
+
+#ifndef _CADENA_PARTICIPANT_H_
+#define _CADENA_PARTICIPANT_H_
+
+#include <fastrtps/fastrtps_fwd.h>
+#include "dsrdeltaPubSubTypes.h"
+#include "dsrpublisher.h"
+#include "dsrsubscriber.h"
+
+class DSRParticipant
+{
+public:
+	DSRParticipant();
+	virtual ~DSRParticipant();
+	std::tuple<bool, eprosima::fastrtps::Participant *> init();
+	void run();
+	eprosima::fastrtps::rtps::GUID_t getID() const ;
+
+private:
+	eprosima::fastrtps::Participant *mp_participant;
+    eprosima::fastrtps::Publisher *mp_publisher;
+    eprosima::fastrtps::Subscriber *mp_subscriber;
+	DSRDeltaPubSubType dsrdeltaType;
+
+};
+
+#endif // _CADENA_Participant_H_
