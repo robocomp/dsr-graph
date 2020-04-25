b094c6e4f78f89684b8f145040d8785320f50416
pbustos <pbustos@unex.es>
2020-04-25 17:53:40 +0200

    added initial test for creation and removal of nodes

--- components/crdt_rtps_dsr_strings/src/specificworker.cpp
+++ components/crdt_rtps_dsr_strings/src/specificworker.cpp
@@ -30,7 +30,7 @@ SpecificWorker::SpecificWorker(TuplePrx tprx) : GenericWorker(tprx) {
 */
 SpecificWorker::~SpecificWorker() {
     std::cout << "Destroying SpecificWorker" << std::endl;
-    gcrdt.reset();
+    G.reset();
 
 }
 
@@ -47,27 +47,27 @@ void SpecificWorker::initialize(int period) {
     std::cout << "Initialize worker" << std::endl;
 
     // create graph
-    gcrdt = std::make_shared<CRDT::CRDTGraph>(0, agent_name, agent_id); // Init nodes
+    G = std::make_shared<CRDT::CRDTGraph>(0, agent_name, agent_id); // Init nodes
     
     // read graph content from file
     if(read_file)
     {
-        gcrdt->read_from_file("grafo.xml");
-        gcrdt->start_fullgraph_server_thread();
-        gcrdt->start_subscription_thread(true);
+        G->read_from_file("grafo.xml");
+        G->start_fullgraph_server_thread();     // to receive requests form othe starting agents
+        G->start_subscription_thread(true);     // regular subscription to deltas
 
     }
     else
     {
-        gcrdt->start_fullgraph_request_thread();
+        G->start_fullgraph_request_thread();    // for agents that want to request the graph for other agent
         //Si se piede el grafo no hace falta iniciar subscription thread, se inicia al sincronizar.
     }
 
     //sleep(TIMEOUT);
     qDebug() << __FUNCTION__ << "Graph loaded";       
     
-    //gcrdt->start_subscription_thread(false);
-    //gcrdt->print();
+    //G->start_subscription_thread(false);
+    //G->print();
     
     // GraphViewer creation
     graph_viewer = std::make_unique<DSR::GraphViewer>(std::shared_ptr<SpecificWorker>(this));
@@ -79,10 +79,9 @@ void SpecificWorker::initialize(int period) {
     dist = std::uniform_real_distribution((float)-40.0, (float)40.0);
     randomNode = std::uniform_int_distribution((int)100, (int)140.0);
     random_selector = std::uniform_int_distribution(0,1);
-    node_selector = std::uniform_int_distribution(5000,6000);
     //timer.start(300)
 
-    // threads
+    // threads for testing concurrent accesses inside one agent
     threads.resize(1);
     for(int i=0; auto &t : threads)
         t = std::move(std::thread(&SpecificWorker::test_create_or_remove_node, this, i++));
@@ -95,7 +94,7 @@ void SpecificWorker::initialize(int period) {
     qDebug() << __FUNCTION__ << "Threads finished";       
 
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[µs]" << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 }
 
 
@@ -107,11 +106,32 @@ void SpecificWorker::compute()
    // test_node_random();
 }
 
+// create and insert a new id in the list
+int SpecificWorker::newID()
+{
+    //std::lock_guard<std::mutex>  lock(mut);
+    int l = created_nodos.back();
+    created_nodos.push_back(++l);
+    return l;
+}
+// pick a random id from the list of new ones
+int SpecificWorker::removeID()    
+{
+    //std::lock_guard<std::mutex>  lock(mut);
+    if(created_nodos.size()==1)
+        return -1;
+    auto node_randomizer = std::uniform_int_distribution(0, (int)created_nodos.size()-1);
+    int l = node_randomizer(mt);
+    int val = created_nodos.at(l);
+    created_nodos.erase(created_nodos.begin()+l);
+    return val;
+}
+
 void SpecificWorker::test_create_or_remove_node(int i)
 {
-    static int cont = 0;
+    static int it=0;
     qDebug() << __FUNCTION__ << "Enter thread" << i;
-    while (cont < 100) 
+    while (it++ < 1000) 
     {
         // ramdomly select create or remove
         if(random_selector(mt) == 0)
@@ -120,20 +140,28 @@ void SpecificWorker::test_create_or_remove_node(int i)
             // create node
             Node node;
             node.type("new_type");
-            node.id(5000+cont);
+            auto id = newID();
+            node.id( id );
             node.agent_id(agent_id);
+            auto at = AttribValue(); 
+            at.key("imName"); at.value("caca");
+            node.attrs()["imName"] = at; 
+            
             // insert node
-            qDebug() << gcrdt->insert_or_assign_node(node) << gcrdt->size();
+            auto r = G->insert_or_assign_node(node);
+            if (r)
+                qDebug() << "Created node:" << id << " Total size:" << G->size();
         }
-        // else
-        // {
-        //     qDebug() << __FUNCTION__ << "Remove node";
-        //     int id = node_selector(mt);
-        //     bool r = gcrdt->delete_node(id);
-        //     if(r==false)
-        //         qDebug() << __FUNCTION__ << "Node not found";
-        // }
-        cont++;
+        else
+        {
+            qDebug() << __FUNCTION__ << "Remove node";
+            int id = removeID();
+            if(id>-1)
+            {
+                G->delete_node(id);
+                qDebug() << "Deleted node:" << id << " Total size:" << G->size();
+            }
+        }
     }
 }
 
@@ -141,26 +169,24 @@ void SpecificWorker::test_set_string(int i)
 {
     static int cont = 0;
     qDebug() << __FUNCTION__ << "Enter thread" << i;
-    while (cont < 1000) 
+    while (cont < 10000) 
     {
         // request node
-        Node node = gcrdt->get_node(135);
+        Node node = G->get_node(135);
         if (node.id() == -1) 
             return;
 
         // check for attribute
-        //auto val = gcrdt->get_node_attrib_by_name(node, "String");
-        auto val = std::find_if(node.attrs().begin(), node.attrs().end(), [](const auto element) { return element.key() == "String"; });
-        if (val == node.attrs().end()) 
+         if ( node.attrs().find("String") == node.attrs().end() ) 
             return;
-
-        //std::string str = boost::str(boost::format("%s - %d") % agent_name % cont);
-        std::string str = agent_name + "-" + std::to_string(i) + "_" + std::to_string(cont);
-        val->value(str);
-
-        // reinsert node
-        gcrdt->insert_or_assign_node(node);
-        qDebug() << __FUNCTION__ << "Strings:" << QString::fromStdString(str);       
+        else 
+        {
+            std::string str = agent_name + "-" + std::to_string(i) + "_" + std::to_string(cont);
+            node.attrs()["String"].value() = str;
+            // reinsert node
+            G->insert_or_assign_node(node);
+            qDebug() << __FUNCTION__ << "Strings:" << QString::fromStdString(str);       
+        }
 
         cont++;
     }
@@ -170,12 +196,12 @@ void SpecificWorker::test_set_string(int i)
 //     static int cont = 0;
 //     if (cont<LAPS) {
 //         try {
-//             for (auto x : gcrdt->get_list()) {
+//             for (auto x : G->get_list()) {
 //                 for (auto &[k, v] : x.attrs) {
 //                     if(k == "pos_x" || k == "pos_y") {
 //                         std::string nValue = std::to_string(std::stoi(v.value) + dist(mt));
 //                         cout << "Nodo: "<<x.id<<", antes: "<<v<<", ahora: "<<nValue<<endl;
-//                         gcrdt->add_node_attrib(x.id, k, v.type, nValue, v.length);
+//                         G->add_node_attrib(x.id, k, v.type, nValue, v.length);
 //                     }
 //                 }
 //             }
@@ -183,7 +209,7 @@ void SpecificWorker::test_set_string(int i)
 //             cont++;
 // //            auto toDelete = randomNode(mt);
 // //            std::cout<<"Deleting.... "<<toDelete<<std::endl;
-// //            gcrdt->delete_node(toDelete);
+// //            G->delete_node(toDelete);
 //         }
 //         catch (const Ice::Exception &e) {
 //             std::cout << "Error reading from Laser" << e << std::endl;
@@ -193,7 +219,7 @@ void SpecificWorker::test_set_string(int i)
 // //        auto to_delete = randomNode(mt);
 // ////        int to_delete = 118;
 // //        std::cout<<"Antes "<<to_delete<<std::endl;
-// //        gcrdt->delete_node(to_delete);
+// //        G->delete_node(to_delete);
 // //        std::cout<<"Fin "<<std::endl;
 //         cont++;
 //     } else
@@ -205,15 +231,15 @@ void SpecificWorker::test_set_string(int i)
 //     if (cont<NODES) {
 //         try {
 //             int to_move = randomNode(mt);
-//             if (gcrdt->in(to_move))
+//             if (G->in(to_move))
 //             {
 //                 std::cout << "[" << cont << "] to_move: " << to_move << std::endl;
-//                 float p_x = gcrdt->get_node_attrib_by_name<float>(to_move, "pos_x");
+//                 float p_x = G->get_node_attrib_by_name<float>(to_move, "pos_x");
 //                 p_x += dist(mt);
-//                 float p_y = gcrdt->get_node_attrib_by_name<float>(to_move, "pos_y");
+//                 float p_y = G->get_node_attrib_by_name<float>(to_move, "pos_y");
 //                 p_y += dist(mt);
-//                 gcrdt->add_node_attrib(to_move, "pos_x", p_x);
-//                 gcrdt->add_node_attrib(to_move, "pos_y", p_y);
+//                 G->add_node_attrib(to_move, "pos_x", p_x);
+//                 G->add_node_attrib(to_move, "pos_y", p_y);
 //             }
 //         } catch (const std::exception &e) {
 //             std::cout << "EXCEPTION: " << __FILE__ << " " << __FUNCTION__ << ":" << __LINE__ << " " << e.what()
@@ -232,7 +258,7 @@ void SpecificWorker::test_set_string(int i)
 //                 auto test = Node{
 //                         "foo_id:" + std::to_string(cont) + "_laps:" + std::to_string(laps) + "_" + agent_name, cont};
 // //            std::cout <<" New node: "<< test << std::endl;
-//                 gcrdt->insert_or_assign(cont, test);
+//                 G->insert_or_assign(cont, test);
 //             }
 //             catch (const std::exception &ex) { cerr << __FUNCTION__ << " -> " << ex.what() << std::endl; }
 
@@ -241,7 +267,7 @@ void SpecificWorker::test_set_string(int i)
 //                 laps++;
 //             }
 //         } else if (laps == LAPS) {
-// //            gcrdt->print();
+// //            G->print();
 //             laps++;
 //         } else
 //             sleep(5);
