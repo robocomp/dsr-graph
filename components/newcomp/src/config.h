43ee8cc63eab0d4a131b28d74f4c15ae9c52f21d
Juan Carlos García García <juancarlos97gg@gmail.com>
2020-04-30 09:29:01 +0200

    Remove old files

--- components/newcomp/src/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-// Comment out this line if your application has a QtGui
-
-#define PROGRAM_NAME    "NewComp"
-#define SERVER_FULL_NAME   "RoboComp NewComp:: NewComp"
-
-#endif
