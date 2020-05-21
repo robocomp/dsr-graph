623322532adfce73b9f112b3ea2ae85a42cb1c2e
rcintas <rcitnas@unex.es>
2020-05-21 19:34:01 +0200

    Component creation

---
+++ components/change_graph/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+#define USE_QTGUI
+
+
+#define PROGRAM_NAME    "change_graph"
+#define SERVER_FULL_NAME   "RoboComp change_graph:: change_graph"
+
+#endif
---
+++ components/change_graph/src/specificworker.cpp
@@ -0,0 +1,64 @@
+/*
+ *    Copyright (C)2018 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+#include <thread>
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(TuplePrx tprx) : GenericWorker(tprx) {
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker() {
+    std::cout << "Destroying SpecificWorker" << std::endl;
+    //G.reset();
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params) 
+{
+
+    agent_id = stoi(params["agent_id"].value);
+    dsr_output_file = params["dsr_output_file"].value;
+    //dsr_input_file = params["dsr_input_file"].value;
+    test_output_file = params["test_output_file"].value;
+    dsr_input_file = params["dsr_input_file"].value;    
+    return true;
+}
+
+void SpecificWorker::initialize(int period) 
+{
+    std::cout << "Initialize worker" << std::endl;
+
+    // create graph
+    G = std::make_shared<CRDT::CRDTGraph>(0, agent_name, agent_id, ""); // Init nodes
+
+     
+    // Graph viewer
+//	graph_viewer = std::make_unique<DSR::GraphViewer>(G);
+}
+
+void SpecificWorker::compute()
+{
+
+}
+
+
