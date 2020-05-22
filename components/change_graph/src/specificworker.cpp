4fc6b85f5a556f97111184dfdb30b18c7e66e6d0
rcintas <rcitnas@unex.es>
2020-05-22 18:13:27 +0200

    Update edge values

--- components/change_graph/src/specificworker.cpp
+++ components/change_graph/src/specificworker.cpp
@@ -62,9 +62,22 @@ void SpecificWorker::initialize(int period)
         QVariant data;
         data.setValue(node);
 	    this->node_cb->addItem(QString::fromStdString(node.name()), data);
+
+        //edges
+        for(const auto &[key, edge] : node.fano())
+        {
+            QVariant edge_data;
+            edge_data.setValue(edge);
+            QString from = QString::fromStdString(G->get_node(edge.from()).value().name());
+            QString to = QString::fromStdString(G->get_node(edge.to()).value().name());
+            QString name = from + "_" + to + "_" + QString::fromStdString(edge.type());
+            this->edge_cb->addItem(name, edge_data);
+        }
+
     }
 
     node_attrib_tw->setColumnCount(2);
+    edge_attrib_tw->setColumnCount(2);
     QStringList horzHeaders;
     horzHeaders <<"Attribute"<< "Value";
     node_attrib_tw->setHorizontalHeaderLabels( horzHeaders );
@@ -75,6 +88,8 @@ void SpecificWorker::initialize(int period)
 
     connect(node_cb, SIGNAL(currentIndexChanged(int)), this, SLOT(change_node_slot(int)));
     connect(save_node_pb, SIGNAL(clicked()), this, SLOT(save_node_slot()));
+    connect(edge_cb, SIGNAL(currentIndexChanged(int)), this, SLOT(change_edge_slot(int)));
+    connect(save_edge_pb, SIGNAL(clicked()), this, SLOT(save_edge_slot()));
 }
 
 
@@ -88,14 +103,25 @@ void SpecificWorker::compute()
 void SpecificWorker::change_node_slot(int id)
 {
     qDebug()<<id;
-    node_attrib_tw->setRowCount(0);
-
     Node node = node_cb->itemData(id).value<Node>();
-    
-    for(auto [key, value] : node.attrs())
+
+    fill_table(node_attrib_tw, node.attrs());
+}
+
+void SpecificWorker::change_edge_slot(int id)
+{
+    Edge edge = edge_cb->itemData(id).value<Edge>();
+    fill_table(edge_attrib_tw, edge.attrs());
+}
+
+
+void SpecificWorker::fill_table(QTableWidget *table_widget, std::map<std::string, Attrib> attribs)
+{
+    table_widget->setRowCount(0);
+    for(auto [key, value] : attribs)
     {
-        node_attrib_tw->insertRow( node_attrib_tw->rowCount() );
-        node_attrib_tw->setItem(node_attrib_tw->rowCount()-1, 0, new QTableWidgetItem(QString::fromStdString(key)));
+        table_widget->insertRow( table_widget->rowCount() );
+        table_widget->setItem(table_widget->rowCount()-1, 0, new QTableWidgetItem(QString::fromStdString(key)));
         QVariant variant;
         switch (value.value()._d())
         {
@@ -108,44 +134,67 @@ void SpecificWorker::change_node_slot(int id)
             case 2:
                 variant = std::round(static_cast<double>(value.value().fl()) *1000000)/ 1000000 ;
                 break;
-                    
+            case 3:
+                if (!value.value().float_vec().empty())
+                {
+                    QString aux;
+                    for(std::size_t i = 0; i < value.value().float_vec().size(); ++i)
+                    {
+                        aux += QString::number(value.value().float_vec()[i]);
+                        if (i != value.value().float_vec().size()-1)
+                            aux += ",";
+                    }
+                    variant = aux;
+                }
+                break;
             case 4:
                 variant = value.value().bl();
                 break;
         }
-        node_attrib_tw->setItem(node_attrib_tw->rowCount()-1, 1, new QTableWidgetItem(variant.toString()));
-
+        table_widget->setItem(table_widget->rowCount()-1, 1, new QTableWidgetItem(variant.toString()));
     }
+}
 
-    std::cout<<node.name()<<std::endl;
 
+std::map<std::string, Attrib> SpecificWorker::get_table_content(QTableWidget *table_widget, std::map<std::string, Attrib> attrs)
+{
+    for (int row=0; row < table_widget->rowCount(); row++)
+    {
+        std::string key = table_widget->item(row, 0)->text().toStdString();
+        QString value = table_widget->item(row, 1)->text();
+        switch(attrs[key].value()._d())
+        {
+            case 0: 
+                attrs[key].value().str(value.toStdString());       
+                break;
+            case 1:
+                attrs[key].value().dec(value.toInt());       
+                break;
+            case 2:
+                attrs[key].value().fl(value.toFloat());       
+                break;
+            case 4:
+                attrs[key].value().bl(value.contains("true"));       
+                break;
+            case 3:
+                std::vector<float> r;
+                for (auto element: value.split(','))
+                    r.push_back(element.toFloat());
+                
+                attrs[key].value().float_vec(r);       
+                break;                
+        }       
+    }
+    return attrs;
 }
 
 
 void SpecificWorker::save_node_slot()
 {
     Node node = node_cb->itemData(node_cb->currentIndex()).value<Node>();
-    for (int row=0; row < node_attrib_tw->rowCount(); row++)
-    {
-        std::string key = node_attrib_tw->item(row, 0)->text().toStdString();
-        QString value = node_attrib_tw->item(row, 1)->text();
-        switch(node.attrs()[key].value()._d())
-        {
-            case 0: 
-                node.attrs()[key].value().str(value.toStdString());       
-                break;
-            case 1:
-                node.attrs()[key].value().dec(value.toInt());       
-                break;
-            case 2:
-                node.attrs()[key].value().fl(value.toFloat());       
-                break;
-                    
-            case 4:
-                node.attrs()[key].value().bl(value.contains("true"));       
-                break;
-        }       
-    }
+
+    std::map<std::string, Attrib> new_attrs = get_table_content(node_attrib_tw, node.attrs());
+    node.attrs(new_attrs);
     
     if(G->insert_or_assign_node(node))
         qDebug()<<"Node saved";
@@ -153,21 +202,21 @@ void SpecificWorker::save_node_slot()
     {
         qDebug()<<"Error saving node";
     }
-    
-
 }
 
-/*        case 0: 
-                G->insert_or_assign_attrib_by_name(node, key, value.toStdString());       
-                break;
-            case 1:
-                G->insert_or_assign_attrib_by_name(node, key, value.toInt());       
-                break;
-            case 2:
-                G->insert_or_assign_attrib_by_name(node, key, value.toFloat()); 
-                break;
-                    
-            case 4:
-                G->insert_or_assign_attrib_by_name(node, key, value.contains("true")); 
-                break;
-                */
+
+void SpecificWorker::save_edge_slot()
+{
+    Edge edge = edge_cb->itemData(edge_cb->currentIndex()).value<Edge>();
+
+    std::map<std::string, Attrib> new_attrs = get_table_content(edge_attrib_tw, edge.attrs());
+    edge.attrs(new_attrs);
+    
+    if(G->insert_or_assign_edge(edge))
+        qDebug()<<"Edge saved";
+    else
+    {
+        qDebug()<<"Error saving edge";
+    }
+  
+}
