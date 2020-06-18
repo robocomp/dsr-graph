95bca386f0775c7c9dbdc796e5a4d3aba28b537e
Juan Carlos García García <juancarlos97gg@gmail.com>
2020-06-18 09:57:31 +0200

    Message for join signals

--- components/crdt_rtps_dsr_tests/src/specificworker.cpp
+++ components/crdt_rtps_dsr_tests/src/specificworker.cpp
@@ -69,8 +69,8 @@ void SpecificWorker::initialize(int period) {
     //G->print();
 
     // Graph viewer
-    using opts = DSR::GraphViewer::View;
-    graph_viewer = std::make_unique<DSR::GraphViewer>(this, G, std::list<opts>{opts::Scene, opts::OSG});
+    using opts = DSR::GraphViewer::view;
+    graph_viewer = std::make_unique<DSR::GraphViewer>(this, G, opts::scene|opts::osg|opts::graph|opts::tree);
 
 
 
