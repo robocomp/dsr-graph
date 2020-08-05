d728002a09babf9fe5a00f68693f323890d85783
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-08-05 21:00:50 +0200

    [components] More minor bug fixes.

--- components/people_to_dsr/src/specificworker.cpp
+++ components/people_to_dsr/src/specificworker.cpp
@@ -59,7 +59,7 @@ void SpecificWorker::initialize(int period)
 
 void SpecificWorker::compute()
 {
-    if(auto pdata = people_data_buffer.get(); pdata.has_value())
+    if(auto pdata = people_data_buffer.try_get(); pdata.has_value())
         process_people_data(pdata.value());
     //check people thas has not been seen
     check_unseen_people();
