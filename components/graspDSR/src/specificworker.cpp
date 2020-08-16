279a3ef12278df407c02b8fd2395d762af74fd82
darkgeek <mohamedshawky911@gmail.com>
2020-08-16 04:24:54 +0200

    complete estimated poses reprojection in graspDSR

--- components/graspDSR/src/specificworker.cpp
+++ components/graspDSR/src/specificworker.cpp
@@ -1,5 +1,5 @@
 /*
- *    Copyright (C) 2020 by YOUR NAME HERE
+ *    Copyright (C) 2020 by Mohamed Shawky
  *
  *    This file is part of RoboComp
  *
@@ -99,22 +99,20 @@ void SpecificWorker::compute()
     RoboCompCameraRGBDSimple::TImage rgb = get_rgb_from_G();
     RoboCompCameraRGBDSimple::TDepth depth = get_depth_from_G();
     // call pose estimation on RGBD and receive estimated poses
-    RoboCompObjectPoseEstimationRGBD::PoseType pose;
+    RoboCompObjectPoseEstimationRGBD::PoseType poses;
     try
     {
-        pose = this->objectposeestimationrgbd_proxy->getObjectPose(rgb, depth);
+        poses = this->objectposeestimationrgbd_proxy->getObjectPose(rgb, depth);
     }
     catch (const Ice::Exception &e)
     {
         std::cout << e << " No RoboCompPoseEstimation component found" << std::endl;
     }
-    // re-project estimated poses into world coordinates
-    // inject final poses into the graph
-    // compute arm pose from object pose
-        // inject arm pose into graph
-        // move arm
-        // check if target not reached
-        // repeat
+    if (poses.size() != 0)
+    {
+        // inject estimated poses into graph
+        this->inject_estimated_poses(poses);
+    }
 }
 
 RoboCompCameraRGBDSimple::TImage SpecificWorker::get_rgb_from_G()
@@ -186,7 +184,7 @@ RoboCompCameraRGBDSimple::TDepth SpecificWorker::get_depth_from_G()
             depth.cameraID = cam_id.value();
             depth.focalx = focalx.value();
             depth.focaly = focaly.value();
-            depth.depthFactor = depth_factor.value();
+            depth.depthFactor = depth_factor.value(); // set to 0.1 for viriato_head_camera_sensor
             depth.alivetime = alivetime.value();
 
             return depth;
@@ -201,6 +199,54 @@ RoboCompCameraRGBDSimple::TDepth SpecificWorker::get_depth_from_G()
     {
         qFatal("Terminate in Compute. No node rgbd found");
     }
+}
+
+void SpecificWorker::inject_estimated_poses(RoboCompObjectPoseEstimationRGBD::PoseType poses)
+{
+    // get innermodel sub-API
+    auto innermodel = G->get_inner_api();
+    // loop over each estimated object pose
+    for (auto pose : poses)
+    {
+        // convert quaternions into euler angles
+        vector<float> quat{pose.qx, pose.qy, pose.qz, pose.qw};
+        vector<float> angles = this->quat_to_euler(quat);
+
+        // re-project estimated poses into world coordinates
+        QVec orig_point = QVec(6);
+        orig_point.setItem(0, pose.x);
+        orig_point.setItem(1, pose.y);
+        orig_point.setItem(2, pose.z);
+        orig_point.setItem(3, angles.at(0));
+        orig_point.setItem(4, angles.at(1));
+        orig_point.setItem(5, angles.at(2));
+        auto final_pose = innermodel->transform("world", orig_point, "camera_pose");
+    }
+}
+
+vector<float> SpecificWorker::quat_to_euler(vector<float> quat)
+{
+    // euler angles vector
+    vector<float> angles;
+
+    // roll (x-axis rotation)
+    double sinr_cosp = 2 * (quat.at(3) * quat.at(0) + quat.at(1) * quat.at(2));
+    double cosr_cosp = 1 - 2 * ( quat.at(0) *  quat.at(0) +  quat.at(1) *  quat.at(1));
+    angles.push_back(std::atan2(sinr_cosp, cosr_cosp));
+
+    // pitch (y-axis rotation)
+    double sinp = 2 * (quat.at(3) * quat.at(1) - quat.at(2) * quat.at(0));
+    if (std::abs(sinp) >= 1)
+        angles.push_back(std::copysign(M_PI / 2, sinp)); // use 90 degrees if out of range
+    else
+        angles.push_back(std::asin(sinp));
+
+    // yaw (z-axis rotation)
+    double siny_cosp = 2 * (quat.at(3) * quat.at(2) + quat.at(0) * quat.at(1));
+    double cosy_cosp = 1 - 2 * (quat.at(1) * quat.at(1) + quat.at(2) * quat.at(2));
+    angles.push_back(std::atan2(siny_cosp, cosy_cosp));
+
+    return angles;
 }
 
 int SpecificWorker::startup_check()
