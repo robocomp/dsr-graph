f92d878cc4b87ead06a07011de248738ffae4a65
darkgeek <mohamedshawky911@gmail.com>
2020-08-16 04:24:54 +0200

    complete estimated poses reprojection in graspDSR

--- components/graspDSR/src/specificworker.h
+++ components/graspDSR/src/specificworker.h
@@ -1,5 +1,5 @@
 /*
- *    Copyright (C) 2020 by YOUR NAME HERE
+ *    Copyright (C) 2020 by Mohamed Shawky
  *
  *    This file is part of RoboComp
  *
@@ -64,8 +64,16 @@ private:
 	bool startup_check_flag;
 
 	// Pose Estimation & Grasping
+
+	// Projection utilities
+	vector<float> quat_to_euler(vector<float> quat);
+
+	// G read utilities
 	RoboCompCameraRGBDSimple::TImage get_rgb_from_G();
 	RoboCompCameraRGBDSimple::TDepth get_depth_from_G();
+
+	// G injection utilities
+	void inject_estimated_poses(RoboCompObjectPoseEstimationRGBD::PoseType poses);
 };
 
 #endif
