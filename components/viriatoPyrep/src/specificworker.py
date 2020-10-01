[viriatoPyrep] Fix robot rotation to match coppelia initial position

components/viriatoPyrep/src/specificworker.py
@@ -229,7 +229,7 @@ class SpecificWorker(GenericWorker):
             isMoving = np.abs(linear_vel[0]) > 0.01 or np.abs(linear_vel[1]) > 0.01 or np.abs(ang_vel[2]) > 0.01
             self.bState = RoboCompGenericBase.TBaseState(x=pose[0] * 1000,
                                                          z=pose[1] * 1000,
-                                                         alpha=pose[2],
+                                                         alpha=pose[2] - 1.570972,
                                                          advVx=linear_vel[0] * 1000,
                                                          advVz=linear_vel[1] * 1000,
                                                          rotV=ang_vel[2],