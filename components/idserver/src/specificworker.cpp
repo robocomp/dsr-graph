ef2fdb431829840dd0e63a55bafa0477edfe5a5a
pbustos <pbustos@unex.es>
2020-10-05 14:05:51 +0200

    change name

--- components/idserver/src/specificworker.cpp
+++ components/idserver/src/specificworker.cpp
@@ -70,7 +70,9 @@ void SpecificWorker::initialize(int period)
         std::cout << __FUNCTION__ << "Graph loaded" << std::endl;
         //check RT tree
         check_rt_tree(G->get_node_root().value());
+
         G->set_ignored_attributes<rgb_att, dists_att, angles_att, img_depth>();
+
         // Graph viewer
         using opts = DSR::DSRViewer::view;
 		int current_opts = tree_view | graph_view | qscene_2d_view | osg_3d_view;
