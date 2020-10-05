d07e5f7dcf0343a8d07024f567ebd4a04bbb90f2
pbustos <pbustos@unex.es>
2020-10-05 08:51:31 +0200

    added viriato_graph_names

---
+++ etc/viriato_graph_names.h
@@ -0,0 +1,23 @@
+//////////////////////////////////////////
+// Node and edges names for Viriato world
+//////////////////////////////////////////
+
+// NODES
+const std::string robot_name = "omnirobot";
+const std::string world_name = "world";
+const std::string floor_name = "infiniteFloor";
+const std::string viriato_head_camera_pan_tilt = "viriato_head_camera_pan_tilt";
+const std::string viriato_head_camera_pan_joint = "viriato_head_camera_pan_joint";
+const std::string viriato_head_camera_tilt_joint = "viriato_head_camera_tilt_joint";
+const std::string viriato_head_camera_name = "viriato_head_camera_sensor";
+const std::string laser_name = "laser";
+const std::string glass_1_name= "glass_1";
+
+//NODE TYPES
+const std::string path_to_target_type = "path_to_target";
+const std::string intention_type = "intention";
+
+// EDGES TYPES
+const std::string think_type = "thinks";
+
+
