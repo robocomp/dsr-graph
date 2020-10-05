07b7c00760ae5b30740eabbd18ca6a495c8bcc29
pbustos <pbustos@unex.es>
2020-10-05 12:15:10 +0200

    bug

--- components/missionDSR/src/specificworker.cpp
+++ components/missionDSR/src/specificworker.cpp
@@ -188,11 +188,11 @@ std::optional<Node> SpecificWorker::get_intent_node(bool create)
         if(auto robot = G->get_node(robot_name); robot.has_value())
         {
             Node node;
-            node.type("intention");
+            node.type(intention_type);
             G->add_or_modify_attrib_local<parent_att>(node,  robot.value().id());
             G->add_or_modify_attrib_local<level_att>(node, G->get_node_level(robot.value()).value() + 1);
-            G->add_or_modify_attrib_local<pos_x_att>(path_to_target_node, (float)-90);
-            G->add_or_modify_attrib_local<pos_y_att>(path_to_target_node, (float)-354;
+            G->add_or_modify_attrib_local<pos_x_att>(node, (float)-90);
+            G->add_or_modify_attrib_local<pos_y_att>(node, (float)-354);
             try
             {     
                 std::optional<int> new_id = G->insert_node(node);
