f9fb4e8e4c04cc9551ae1eb9b8219a301ab14ecf
pbustos <pbustos@unex.es>
2020-10-05 21:38:21 +0200

    regenerated

---
+++ components/path_follower/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+#define USE_QTGUI
+
+
+#define PROGRAM_NAME    "path_controller"
+#define SERVER_FULL_NAME   "RoboComp path_controller:: path_controller"
+
+#endif
---
+++ components/path_follower/src/ui_mainUI.h
@@ -0,0 +1,86 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainUI.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.8
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINUI_H
+#define UI_MAINUI_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_guiDlg
+{
+public:
+    QAction *actionSave;
+    QAction *actionSimulate;
+    QAction *actionSaveToFile;
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QMenu *menuForces;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *guiDlg)
+    {
+        if (guiDlg->objectName().isEmpty())
+            guiDlg->setObjectName(QString::fromUtf8("guiDlg"));
+        guiDlg->resize(800, 600);
+        actionSave = new QAction(guiDlg);
+        actionSave->setObjectName(QString::fromUtf8("actionSave"));
+        actionSimulate = new QAction(guiDlg);
+        actionSimulate->setObjectName(QString::fromUtf8("actionSimulate"));
+        actionSimulate->setCheckable(true);
+        actionSaveToFile = new QAction(guiDlg);
+        actionSaveToFile->setObjectName(QString::fromUtf8("actionSaveToFile"));
+        centralwidget = new QWidget(guiDlg);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        guiDlg->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(guiDlg);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 22));
+        menuForces = new QMenu(menubar);
+        menuForces->setObjectName(QString::fromUtf8("menuForces"));
+        guiDlg->setMenuBar(menubar);
+        statusbar = new QStatusBar(guiDlg);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        guiDlg->setStatusBar(statusbar);
+
+        menubar->addAction(menuForces->menuAction());
+
+        retranslateUi(guiDlg);
+
+        QMetaObject::connectSlotsByName(guiDlg);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *guiDlg)
+    {
+        guiDlg->setWindowTitle(QApplication::translate("guiDlg", "DSRGraph", nullptr));
+        actionSave->setText(QApplication::translate("guiDlg", "Save", nullptr));
+#ifndef QT_NO_TOOLTIP
+        actionSave->setToolTip(QApplication::translate("guiDlg", "<html><head/><body><p>Save file in JSON format</p></body></html>", nullptr));
+#endif // QT_NO_TOOLTIP
+        actionSimulate->setText(QApplication::translate("guiDlg", "Simulate", nullptr));
+        actionSaveToFile->setText(QApplication::translate("guiDlg", "Save", nullptr));
+        menuForces->setTitle(QApplication::translate("guiDlg", "Forces", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class guiDlg: public Ui_guiDlg {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINUI_H
