55cf0bf454d0d4d9e5b5bbde111e66fc3de7a918
pbustos <pbustos@unex.es>
2020-10-10 00:14:30 +0200

    almost there

---
+++ components/octomap/src/specificworker.cpp
@@ -0,0 +1,174 @@
+/*
+ *    Copyright (C) 2020 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+#include <cppitertools/zip.hpp>
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(TuplePrx tprx, bool startup_check) : GenericWorker(tprx)
+{
+	this->startup_check_flag = startup_check;
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+	G->write_to_json_file("./"+agent_name+".json");
+	G.reset();
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+//	THE FOLLOWING IS JUST AN EXAMPLE
+//	To use innerModelPath parameter you should uncomment specificmonitor.cpp readConfig method content
+//	try
+//	{
+//		RoboCompCommonBehavior::Parameter par = params.at("InnerModelPath");
+//		std::string innermodel_path = par.value;
+//		innerModel = std::make_shared(innermodel_path);
+//	}
+//	catch(const std::exception &e) { qFatal("Error reading config params"); }
+
+	agent_name = params["agent_name"].value;
+	agent_id = stoi(params["agent_id"].value);
+
+	tree_view = params["tree_view"].value == "true";
+	graph_view = params["graph_view"].value == "true";
+	qscene_2d_view = params["2d_view"].value == "true";
+	osg_3d_view = params["3d_view"].value == "true";
+
+	return true;
+}
+
+void SpecificWorker::initialize(int period)
+{
+	std::cout << "Initialize worker" << std::endl;
+	this->Period = period;
+	if(this->startup_check_flag)
+	{
+		this->startup_check();
+	}
+	else
+	{
+		timer.start(Period);
+		// create graph
+		G = std::make_shared<DSR::DSRGraph>(0, agent_name, agent_id, "", dsrgetid_proxy); // Init nodes
+		std::cout<< __FUNCTION__ << "Graph loaded" << std::endl;  
+
+		// Graph viewer
+		using opts = DSR::DSRViewer::view;
+		int current_opts = 0;
+		opts main = opts::none;
+		if(tree_view)
+		{
+		    current_opts = current_opts | opts::tree;
+		}
+		if(graph_view)
+		{
+		    current_opts = current_opts | opts::graph;
+		    main = opts::graph;
+		}
+		if(qscene_2d_view)
+		{
+		    current_opts = current_opts | opts::scene;
+		}
+		if(osg_3d_view)
+		{
+		    current_opts = current_opts | opts::osg;
+		}
+		graph_viewer = std::make_unique<DSR::DSRViewer>(this, G, current_opts, main);
+		setWindowTitle(QString::fromStdString(agent_name + "-") + QString::number(agent_id));
+
+        //Inner Api
+        inner_eigen = G->get_inner_eigen_api();
+
+        // Octomap
+		octo = std::make_unique<octomap::OcTree>(1);
+		// initializa from file or G
+		initialize_octomap();
+
+		this->Period = period;
+		timer.start(Period);
+	}
+}
+
+void SpecificWorker::compute()
+{
+    if( const auto laser_data = laser_buffer.try_get(); laser_data.has_value())
+    {
+        for(const auto &point : laser_data.value())
+            octo->updateNode(point, true);
+
+        // if changes, publish in G
+    }
+}
+
+void SpecificWorker::initialize_octomap()
+{
+
+}
+///////////////////////////////////////////////////////////////////
+/// Asynchronous changes on G nodes from G signals
+///////////////////////////////////////////////////////////////////
+void SpecificWorker::update_node_slot(const std::int32_t id, const std::string &type)
+{
+    if (type == laser_type)    // Laser node updated
+    {
+        //qInfo() << __FUNCTION__ << " laser node change";
+        if( auto node = G->get_node(id); node.has_value())
+        {
+            auto angles = G->get_attrib_by_name<laser_angles_att>(node.value());
+            auto dists = G->get_attrib_by_name<laser_dists_att>(node.value());
+            if(dists.has_value() and angles.has_value())
+            {
+                if(dists.value().get().empty() or angles.value().get().empty()) return;
+                //qInfo() << __FUNCTION__ << dists->get().size();
+                laser_buffer.put(std::make_tuple(angles.value().get(), dists.value().get()),
+                                 [this](const LaserData &in, std::vector<octomap::point3d> &out) {
+                                     const auto &[angles, dists] = in;
+                                     for (const auto &[angle, dist] : iter::zip(angles, dists))
+                                     {
+                                         //convert laser polar coordinates to cartesian
+                                         float x = dist * sin(angle);
+                                         float y = dist * cos(angle);
+                                         Mat::Vector3d laserWorld = inner_eigen->transform(world_name,Mat::Vector3d(x, y, 0), laser_name).value();
+                                         out.emplace_back(laserWorld.x(), laserWorld.y(), 10);
+                                     }
+                                 });
+            }
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////77
+int SpecificWorker::startup_check()
+{
+	std::cout << "Startup check" << std::endl;
+	QTimer::singleShot(200, qApp, SLOT(quit()));
+	return 0;
+}
+
+/**************************************/
+// From the RoboCompDSRGetID you can call this methods:
+// this->dsrgetid_proxy->getID(...)
+
