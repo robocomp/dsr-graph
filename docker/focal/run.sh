af2db5017ba82c7a407f8908d8771d19ff1e4f3e
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-02-06 08:18:52 +0100

    [docker] Splitted example into multiple services. Added config files for idserver and viriatoDSR

--- docker/focal/run.sh
+++ docker/focal/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 if [ -z "$COPPELIASIM_ROOT" ]; then
     echo "You need to export \$COPPELIASIM_ROOT to the path where you have installed Coppelia SIM"
@@ -6,4 +7,6 @@ fi
 xhost +"local:docker@"
 docker-compose up &
 source /etc/profile.d/robocomp.sh
-rccd viriatoPyrep && sh run.sh
+old_dir=$(pwd)
+(rccd viriatoPyrep && sh run.sh) &
+cd old_dir
