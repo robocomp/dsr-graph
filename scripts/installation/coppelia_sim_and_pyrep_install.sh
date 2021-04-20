d4c3512d6d3d4e2409f1afa0b52a22586c2f0442
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-04-20 17:26:20 +0200

    Update coppelia_sim_and_pyrep_install.sh
    Removed path for QT Coppelia

--- scripts/installation/coppelia_sim_and_pyrep_install.sh
+++ scripts/installation/coppelia_sim_and_pyrep_install.sh
@@ -6,8 +6,7 @@ export COPPELIASIM_ROOT=$COPPELIA_ROOT_PATH
 export LD_LIBRARY_PATH=$COPPELIA_ROOT_PATH
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIA_ROOT_PATH
 sudo echo "\export COPPELIASIM_ROOT=$COPPELIA_ROOT_PATH
-export LD_LIBRARY_PATH=$COPPELIASIM_ROOT
-export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT" >> /etc/profile.d/coppelia_path.sh
+export LD_LIBRARY_PATH=$COPPELIASIM_ROOT" >> /etc/profile.d/coppelia_path.sh
 sudo ldconfig
 sudo apt-get -y install python3-dev python3-opencv libgl1-mesa-dev
 sudo python3 -mpip install cffi numpy numpy_indexed
