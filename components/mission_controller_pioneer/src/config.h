38e8ecdd2ce4afed5acee75ca8b659f5d8878afa
pbustos <pbustos@unex.es>
2021-07-19 20:02:50 +0200

    added mission_controller_pioneer

---
+++ components/mission_controller_pioneer/src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Comment out this line if your application has a QtGui
+#define USE_QTGUI
+
+
+#define PROGRAM_NAME    "controller_dsr"
+#define SERVER_FULL_NAME   "RoboComp controller_dsr:: controller_dsr"
+
+#endif
