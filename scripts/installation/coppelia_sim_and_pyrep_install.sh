429b21e98a60d587361d31aa7a370d2fa928b8f8
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-09-02 17:45:55 +0200

    Fixing install script
    More fixes
    more fixes
    Coppelia Updated

--- scripts/installation/coppelia_sim_and_pyrep_install.sh
+++ scripts/installation/coppelia_sim_and_pyrep_install.sh
@@ -1,13 +1,12 @@
 set -v
-COPPELIA_ROOT_PATH=$(pwd)/extras/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04
-sudo apt install xz-utils
-mkdir extras && cd extras && wget -c https://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04.tar.xz -O - | sudo tar -xv -J
+COPPELIA_ROOT_PATH=$(pwd)/extras/CoppeliaSim_Edu_V4_2_0_Ubuntu20_04
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install -y xz-utils python3-dev python3-opencv libgl1-mesa-dev x11-apps
+mkdir extras && cd extras && curl https://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_2_0_Ubuntu20_04.tar.xz --output - | sudo tar -xv -J
 export COPPELIASIM_ROOT=$COPPELIA_ROOT_PATH
 export LD_LIBRARY_PATH=$COPPELIA_ROOT_PATH
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIA_ROOT_PATH
-sudo echo "\export COPPELIASIM_ROOT=$COPPELIA_ROOT_PATH
-export LD_LIBRARY_PATH=$COPPELIASIM_ROOT" >> /etc/profile.d/coppelia_path.sh
+echo "export COPPELIASIM_ROOT=$COPPELIA_ROOT_PATH" | sudo tee /etc/profile.d/coppelia_path.sh
 sudo ldconfig
-sudo apt-get -y install python3-dev python3-opencv libgl1-mesa-dev
-sudo python3 -mpip install cffi numpy numpy_indexed
+sudo python3 -mpip install cffi numpy numpy_indexed pytransform3d
 git clone --progress https://github.com/stepjam/PyRep.git && cd PyRep && sudo COPPELIASIM_ROOT=$COPPELIA_ROOT_PATH python3 -m pip install .
