e16430fa98b95ef5a1bb18d8f45559d300829599
Juan Carlos García García <juancarlos97gg@gmail.com>
2021-12-03 09:39:45 +0100

    +Qt3d

--- components/qt3d-pruebas/src/specificworker.h
+++ components/qt3d-pruebas/src/specificworker.h
@@ -34,6 +34,7 @@
 #include "dsr/api/dsr_inner_eigen_api.h"
 #include "dsr/api/dsr_geometry_queries_api.h"
 #include "dsr/api/dsr_camera_api.h"
+#include <dsr/api/GHistorySaver.h>
 
 #include "3D_view.h"
 
@@ -62,6 +63,7 @@ private:
     geometry_queries_api geom;
     std::unique_ptr<DSR::CameraAPI> cam;
     std::shared_ptr<DSR::InnerEigenAPI> inner;
+    //std::unique_ptr<GSerializer> gsr;
     std::unordered_map<std::string, QLabel*> contornos;
     std::unordered_map<std::string, std::unique_ptr<cv::Mat>> contornos_img;
 
@@ -83,6 +85,7 @@ private:
 	void add_or_assign_attrs_slot(std::uint64_t id, const std::map<std::string, DSR::Attribute> &attribs){};
 	void add_or_assign_edge_slot(std::uint64_t from, std::uint64_t to,  const std::string &type);
 
+    void virtual_structure_validation(const std::string& name, const Mat::Vector3d& pos, const std::vector<std::pair<std::string, float>>& near);
 	void del_edge_slot(std::uint64_t from, std::uint64_t to, const std::string &edge_tag){
 	    if (edge_tag == "visible") {
             std::cout << "close" << std::endl;
