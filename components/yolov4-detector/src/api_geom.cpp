bb4bbace15cf60eab3298237de075d738ec63760
AscheWandern <josediazp99@gmail.com>
2022-01-27 12:57:28 +0100

    test yolo json and yolo api

---
+++ components/yolov4-detector/src/api_geom.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alumno on 19/1/22.
+//
+
+#include "api_geom.h"
---
+++ components/yolov4-detector/src/api_geom.h
@@ -0,0 +1,18 @@
+//
+// Created by alumno on 19/1/22.
+//
+
+#ifndef YOLOV4_DETECTOR_API_GEOM_H
+#define YOLOV4_DETECTOR_API_GEOM_H
+#include <vector>
+
+class api_geom
+{
+    static std::vector<> get_large_size_objects();
+    static std::vector<> get_container_objects();
+
+
+};
+
+
+#endif //YOLOV4_DETECTOR_API_GEOM_H
