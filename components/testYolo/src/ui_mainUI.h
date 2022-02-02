62479b4515a90f71885eb59b0bc88f8eb8d75da0
LucasBonilla26 <lubonilla@alumnos.unex.es>
2022-02-02 11:30:34 +0100

    yolo test agent

---
+++ components/testYolo/src/ui_mainUI.h
@@ -0,0 +1,97 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainUI.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.8
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINUI_H
+#define UI_MAINUI_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_guiDlg
+{
+public:
+    QAction *actionStart_Stop;
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QSplitter *splitter_1;
+    QSplitter *splitter_2;
+    QMenuBar *menubar;
+    QMenu *menuSimulation;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *guiDlg)
+    {
+        if (guiDlg->objectName().isEmpty())
+            guiDlg->setObjectName(QString::fromUtf8("guiDlg"));
+        guiDlg->resize(800, 600);
+        actionStart_Stop = new QAction(guiDlg);
+        actionStart_Stop->setObjectName(QString::fromUtf8("actionStart_Stop"));
+        centralwidget = new QWidget(guiDlg);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        splitter_1 = new QSplitter(centralwidget);
+        splitter_1->setObjectName(QString::fromUtf8("splitter_1"));
+        splitter_1->setOrientation(Qt::Horizontal);
+        splitter_2 = new QSplitter(splitter_1);
+        splitter_2->setObjectName(QString::fromUtf8("splitter_2"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(splitter_2->sizePolicy().hasHeightForWidth());
+        splitter_2->setSizePolicy(sizePolicy);
+        splitter_2->setOrientation(Qt::Vertical);
+        splitter_1->addWidget(splitter_2);
+
+        verticalLayout->addWidget(splitter_1);
+
+        guiDlg->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(guiDlg);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 780, 25));
+        menuSimulation = new QMenu(menubar);
+        menuSimulation->setObjectName(QString::fromUtf8("menuSimulation"));
+        guiDlg->setMenuBar(menubar);
+        statusbar = new QStatusBar(guiDlg);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        guiDlg->setStatusBar(statusbar);
+
+        menubar->addAction(menuSimulation->menuAction());
+        menuSimulation->addAction(actionStart_Stop);
+
+        retranslateUi(guiDlg);
+
+        QMetaObject::connectSlotsByName(guiDlg);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *guiDlg)
+    {
+        guiDlg->setWindowTitle(QApplication::translate("guiDlg", "testYolo", nullptr));
+        actionStart_Stop->setText(QApplication::translate("guiDlg", "Start/Stop", nullptr));
+        menuSimulation->setTitle(QApplication::translate("guiDlg", "Simulation", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class guiDlg: public Ui_guiDlg {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINUI_H
